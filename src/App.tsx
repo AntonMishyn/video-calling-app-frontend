df79e4f3009817408b4c7d7290ccd2b9f657f61e
AntonMishyn <mishinaa333@gmial.com>
2023-01-01 22:35:33 -0500

    final

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ enum ConnectionStatus {
   CONNECTED,
 }
 
-const webSocketConnection = new WebSocket("ws://powerful-fjord-54817.herokuapp.com/videochat");
+const webSocketConnection = new WebSocket("ws://localhost:8080/videochat");
 
 export const VideoCall = () => {
   const videoSelf = useRef<HTMLVideoElement | null>(null);
